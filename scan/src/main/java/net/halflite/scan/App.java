793515ef67524e75bf33c83dcf2c7dc73d8c3ed4
haldlite <shingo@halflite.net>
2022-09-09 01:25:41 +0000

    実行時のログを出す

--- scan/src/main/java/net/halflite/scan/App.java
+++ scan/src/main/java/net/halflite/scan/App.java
@@ -1,5 +1,7 @@
 package net.halflite.scan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
@@ -8,15 +10,21 @@ import net.halflite.scan.config.DaggerAppComponent;
 import net.halflite.scan.service.ScanService;
 
 public class App implements RequestHandler<S3Event, Void> {
+  /** logger */
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+  
   private final ScanService scanService;
   
   @Override
   public Void handleRequest(S3Event input, Context context) {
     try {
+      LOG.info("started.");
       this.scanService.execute(input);
       // TODO 何か成功キューを送る
+      LOG.info("completed.");
       return null;
     } catch (Exception e) {
+      LOG.info("error.", e);
       // TODO 何か失敗キューを送る
       return null;
     }
